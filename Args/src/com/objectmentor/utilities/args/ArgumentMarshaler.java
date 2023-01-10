fe6779e6c4a09e1dcee2c9cb3c9b1cd754e70aaa
Neonlab_kr <ksknk67@gmail.com>
2023-01-10 19:18:40 +0900

    Args 구현

---
+++ Args/src/com/objectmentor/utilities/args/ArgumentMarshaler.java
@@ -0,0 +1,6 @@
+package com.objectmentor.utilities.args;
+import java.util.*;
+
+public interface ArgumentMarshaler {
+	void set(Iterator<String> currentArgument) throws ArgsException;
+}
---
+++ Args/src/com/objectmentor/utilities/args/DoubleArgumentMarshaler.java
@@ -0,0 +1,14 @@
+package com.objectmentor.utilities.args;
+
+import java.util.*;
+
+public class DoubleArgumentMarshaler implements ArgumentMarshaler {
+
+	public void set(Iterator<String> currentArgument) throws ArgsException {
+
+	}
+
+	public static double getValue(ArgumentMarshaler argumentMarshaller) {
+		return 0;
+	}
+}
